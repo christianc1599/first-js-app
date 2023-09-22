9d8be0e38c9f05c9b2bfa6955616bd4332e11454
christianc1599 <134043407+christianc1599@users.noreply.github.com>
2023-09-22 09:53:19 -0400

    Pokemon List Array
    I created an array with 3 objects in it. The objects are 3 different Pokemon including their names, heights, and types.

---
+++ js/scripts.js
@@ -0,0 +1,1 @@
+let pokemonList = [{name: "Espurr", height: "1 foot", type: "psychic"},{name: "Mismagius", height: "3 feet", type: "ghost"},{name: "Garchomp", height: "6 feet", type: ["ground","dragon"]}];

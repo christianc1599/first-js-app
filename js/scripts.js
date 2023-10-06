d8eb9a51d9bab92147c1a0d19bfed17e8bcc19fe
christianc1599 <134043407+christianc1599@users.noreply.github.com>
2023-10-06 11:29:30 -0400

    Format Change
    Changed format of array to be easier to read. Placed each array item in separate rows to separate them.

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,4 @@
-let pokemonList = [{name: "Espurr", height: "1 foot", type: "psychic"},{name: "Mismagius", height: "3 feet", type: "ghost"},{name: "Garchomp", height: "6 feet", type: ["ground","dragon"]}];
+let pokemonList = 
+[{name: "Espurr", height: "1 foot", type: "psychic"},
+{name: "Mismagius", height: "3 feet", type: "ghost"},
+{name: "Garchomp", height: "6 feet", type: ["ground","dragon"]}];

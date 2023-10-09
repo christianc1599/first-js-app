ac8e3406c0b7a53895bd942b2078d83ee2ecbe33
christianc1599 <134043407+christianc1599@users.noreply.github.com>
2023-10-09 10:44:57 -0400

    Added a loop
    In this edit, I added a loop to print each pokemon in my array. If the pokemon was less that 6 feet tall, it should print "thats pretty small!" and if it was greater than or equal to 6 feet tall, it should print "thats quite large". I also removed the word "foot" from the array itself since the loop would only recognize the number and not the "foot" or "feet" part of the height. I instead added this to the loop to print "feet".

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,13 @@
-let pokemonList = 
-[{name: "Espurr", height: "1 foot", type: "psychic"},
-{name: "Mismagius", height: "3 feet", type: "ghost"},
-{name: "Garchomp", height: "6 feet", type: ["ground","dragon"]}];
+let pokemonList = [
+{name: "Espurr", height: "1", type: "psychic"},
+{name: "Mismagius", height: "3", type: "ghost"},
+{name: "Garchomp", height: "6", type: ["ground","dragon"]}
+];
+
+for (let i=0; i >= 0; i++){
+    if (pokemonList[i].height > 0 && pokemonList[i].height < 6){
+      document.write(pokemonList[i].name + " Height= " + pokemonList[i].height + " feet tall, thats pretty small!" + " ");
+    } else if (pokemonList[i].height >= 6) {
+        document.write(pokemonList[i].name + " Height= " + pokemonList[i].height + " feet tall, thats quite large!" + " ")
+    }
+  }

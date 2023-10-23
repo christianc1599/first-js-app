5a6245fe543b69e0b69d759868ecbe4920bc68ae
christianc1599 <134043407+christianc1599@users.noreply.github.com>
2023-10-23 13:45:22 -0400

    Added semicolon
    Adjusted code since I forgot to add a semicolon after the second "document.write" line.

--- js/scripts.js
+++ js/scripts.js
@@ -8,6 +8,6 @@ for (let i=0; i >= 0; i++){
     if (pokemonList[i].height > 0 && pokemonList[i].height < 6){
       document.write(pokemonList[i].name + " Height= " + pokemonList[i].height + " feet tall, thats pretty small!" + " ");
     } else if (pokemonList[i].height >= 6) {
-        document.write(pokemonList[i].name + " Height= " + pokemonList[i].height + " feet tall, thats quite large!" + " ")
+        document.write(pokemonList[i].name + " Height= " + pokemonList[i].height + " feet tall, thats quite large!" + " ");
     }
   }

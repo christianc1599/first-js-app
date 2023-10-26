71ebdeca256785c38343534cbac57e79ff1e67c6
christianc1599 <134043407+christianc1599@users.noreply.github.com>
2023-10-26 15:56:40 -0400

    Changed one array object and conditional
    I changed one of the pokemon (Garchomp to Sceptile) in the array to have just one type for now. I kept the height as 6 so that the third array object would still print "feet tall, thats quite large!" I also edited the conditional for the "for" loop so that the code would stop after the third pokemon in the array. Lastly, I changed the height maximum for the "else if" portion of the "for" loop just to make it easier to understand.

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,13 @@
 let pokemonList = [
 {name: "Espurr", height: "1", type: "psychic"},
 {name: "Mismagius", height: "3", type: "ghost"},
-{name: "Garchomp", height: "6", type: ["ground","dragon"]}
+{name: "Sceptile", height: "6", type: "grass"}
 ];
 
-for (let i=0; i >= 0; i++){
+for (let i=0; i < 3; i++){
     if (pokemonList[i].height > 0 && pokemonList[i].height < 6){
       document.write(pokemonList[i].name + " Height= " + pokemonList[i].height + " feet tall, thats pretty small!" + " ");
-    } else if (pokemonList[i].height >= 6) {
+    } else if (pokemonList[i].height > 5) {
         document.write(pokemonList[i].name + " Height= " + pokemonList[i].height + " feet tall, thats quite large!" + " ");
     }
   }

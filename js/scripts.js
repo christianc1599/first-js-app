8cf1e5f81c7bc0d3a1120854721b39ea0ea0c05b
christianc1599 <134043407+christianc1599@users.noreply.github.com>
2023-10-28 13:52:46 -0400

    Updated if/else loop for Pokemon List
    Edited pokemon array so that it would combine both lists of pokemon I made. Also fixed the if/else loop so that if there is a pokemon between 5.1 and 6.9 feet tall that the code would still provide an explanation of the pokemon.

--- js/scripts.js
+++ js/scripts.js
@@ -1,22 +1,23 @@
 let pokemonList = [
 {name: "Espurr", height: "1", type: "psychic"},
 {name: "Mismagius", height: "3", type: "ghost"},
-{name: "Sceptile", height: "6", type: "grass"}
-];
-let pokemonList2 = [
 {name: "Delphox", height: "5", type: "fire"},
 {name: "Shuppet", height: "2", type: "ghost"},
+{name: "Sceptile", height: "6", type: "grass"},
 {name: "Milotic", height: "20", type: "water"}
 ]
 
-function printArrayDetails(list){
-for (let i=0; i < list.length; i++){
-    if (list[i].height > 0 && list[i].height < 6){
-      document.write(list[i].name + " Height= " + list[i].height + " feet tall, thats pretty small!" + " ");
-    } else if (pokemonList[i].height > 5) {
-        document.write(list[i].name + " Height= " + list[i].height + " feet tall, thats quite large!" + " ");
-    }
+for (let i=0; i < pokemonList.length; i++){
+  if (pokemonList[i].height > 0 && pokemonList[i].height <= 5){
+      document.write(pokemonList[i].name + " Height= " + pokemonList[i].height + " feet tall, thats pretty small!" + " ");
+  } else if (pokemonList[i].height > 5) {
+    document.write(pokemonList[i].name + " Height= " + pokemonList[i].height + " feet tall, thats quite large!" + " ");
   }
 }
 
-printArrayDetails(pokemonList);
+/*
+function myLoopFunction(pokemon) {
+    document.write(pokemon.name + ' is ' + pokemon.height + ' feet tall and is a ' + pokemon.type + ' type. ');
+}
+pokemonList.forEach(myLoopFunction);
+*/

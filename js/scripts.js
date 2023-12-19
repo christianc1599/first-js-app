2d71efb1294fc4008af3d4dc5e6d8efae311b121
christianc1599 <134043407+christianc1599@users.noreply.github.com>
2023-12-19 16:02:09 -0500

    Created forEach loop
    I created the for.Each loop and commented out the for loop I had to iterate over each pokemon in my array. I also created an IIFE for practice and commented it out for now.

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,5 @@
+/*let pokemonRespository = (function() {*/
+
 let pokemonList = [
 {name: "Espurr", height: "1", type: "psychic"},
 {name: "Mismagius", height: "3", type: "ghost"},
@@ -6,18 +8,35 @@ let pokemonList = [
 {name: "Sceptile", height: "6", type: "grass"},
 {name: "Milotic", height: "20", type: "water"}
 ]
+/*
+function getAll() {
+    return pokemonList;
+}
 
-for (let i=0; i < pokemonList.length; i++){
+function add (pokemon){
+    return pokemonList.push(pokemon);
+}
+
+return {
+    getAll: getAll,
+    add: add
+}
+
+})()
+
+console.log(pokemonRespository.getAll());
+*/
+
+/*for (let i=0; i < pokemonList.length; i++){
   if (pokemonList[i].height > 0 && pokemonList[i].height <= 5){
       document.write(pokemonList[i].name + " Height= " + pokemonList[i].height + " feet tall, thats pretty small!" + " ");
   } else if (pokemonList[i].height > 5) {
     document.write(pokemonList[i].name + " Height= " + pokemonList[i].height + " feet tall, thats quite large!" + " ");
   }
-}
+}*/
 
-/*
+
 function myLoopFunction(pokemon) {
     document.write(pokemon.name + ' is ' + pokemon.height + ' feet tall and is a ' + pokemon.type + ' type. ');
 }
 pokemonList.forEach(myLoopFunction);
-*/
